b02e15d4db1179c1cf29339727c385f9b9e6a337
Junchao Zheng <L@L-MacBook-Pro.fios-router.home>
2015-12-06 01:34:12 -0500

    data_extract added to general_function_11.6_junchao

--- junchao_code/general_functions.py
+++ junchao_code/general_functions.py
@@ -8,3 +8,16 @@ def get_dictionary(filename):
 	dictionary = pickle.load(open(filename, 'rb'))
 	return dictionary 
 
+def data_extract(data, startyear, startmonth, startday, endyear, endmonth, endday):
+    
+    data_by_year_start = data.loc[data['startyear'] == startyear]
+    data_by_month_start = data_by_year_start[data_by_year_start['startmonth'] == startmonth]
+    data_by_day_start = data_by_month_start[data_by_month_start['startday'] == startday]
+    startindex = data_by_day_start.index[0]
+    
+    data_by_year_end = data.loc[data['startyear'] == endyear]
+    data_by_month_end = data_by_year_end[data_by_year_end['startmonth'] == endmonth]
+    data_by_day_end = data_by_month_end[data_by_month_end['startday'] == endday]
+    endindex = data_by_day_end.index[-1]
+    
+    return data.ix[startindex:endindex, :]
